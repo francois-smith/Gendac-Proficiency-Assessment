827690a80a887da8a965bab9fc0a2a209e2c3d2e
Francois Smith <steamfaffa@gmail.com>
2022-10-08 23:39:04 +0200

    Added Bootstrap JS

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/js/bootstrap.bundle.min";
 import './custom.scss';
 import Dashboard from './Components/Dashboard';
 
