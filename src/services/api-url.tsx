10fcf8ab0b9b87c758f6c216523af829ebbf39f4
Francois Smith <steamfaffa@gmail.com>
2022-10-10 22:49:37 +0200

    Fixed Content Type Not Registering

--- src/services/api-url.tsx
+++ src/services/api-url.tsx
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://gendacproficiencytest.azurewebsites.net/api/ProductsAPI',
     headers: {
-        'Content-type': 'application/json'
+        'Content-Type': 'application/json'
     }
 });
