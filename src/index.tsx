30907e53cb3cd467e8a47c9bf8c8e0c4c6f979ed
Francois Smith <steamfaffa@gmail.com>
2022-10-10 22:47:46 +0200

    Toast Styling

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import 'react-toastify/dist/ReactToastify.css';
 import './custom.scss';
 import Dashboard from './Components/Dashboard';
 
