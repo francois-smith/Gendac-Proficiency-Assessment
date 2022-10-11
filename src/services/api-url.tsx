4f1dcc1550820758e061ff72e0005de0582be77b
Francois Smith <steamfaffa@gmail.com>
2022-10-11 21:45:26 +0200

    Added Docs to Axios Object

--- src/services/api-url.tsx
+++ src/services/api-url.tsx
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
+/**
+ * @description -  Boilerplate axios instance, used to do numerous requests to the API without needing to copy and paste the URL.
+ */
 export default axios.create({
     baseURL: 'https://gendacproficiencytest.azurewebsites.net/api/ProductsAPI',
     headers: {
